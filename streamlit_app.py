a14d18b27afe6c70e64e4e02b388161a4b760fc3
fredericklaud <fredericklaud@gmail.com>
2025-04-24 11:01:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🖥Machine Learning on Rails')
 
 st.info("New century of Machine Learning")
+
+epl_df = pd.read_csv('https://raw.githubusercontent.com/fredericklaud/fl-data/refs/heads/main/England%20CSV.csv')
+with st.expander('EPL Data'):
+  st.write(epl_df)
