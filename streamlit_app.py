9b22a44d40f143e2defb14cf1e9294be3171b9b9
fredericklaud <fredericklaud@gmail.com>
2025-04-24 11:18:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 st.set_page_config(
-    page_title="Unearthly Materials Lab",
+    page_title="Machine Learning Lab",
     page_icon="i",
     layout="wide",
     menu_items={
